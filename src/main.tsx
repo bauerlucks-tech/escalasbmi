3aaa6d822034aea408643b93743f81884946cfc5
lnpott <lnpott@gmail.com>
2026-01-20 21:05:11 -0300

    Fix React 18 createRoot and Tailwind imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,14 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const container = document.getElementById("root");
+if (!container) throw new Error("Root container is missing in index.html");
+
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
